42e26480fc01047f248d60fef5217e8c3e704e18
Sin Jaeho <stra2003@gmail.com>
2022-12-13 15:49:50 +0900

    3.POC API

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 import { ApolloServer, gql } from "apollo-server";
+
 const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 const resolvers = {
   Query: {
-    hello: () => "world",
+    movies: () => [],
+    movie: () => ({ title: "Hi", year: 2022 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
-
-const mutation = {};
 
 const server = new ApolloServer({
   typeDefs,
@@ -19,4 +38,4 @@ const server = new ApolloServer({
 
 server
   .listen()
-  .then(() => console.log("Server is running on https://localhost:4000"));
+  .then(() => console.log("Server is running on http://localhost:4000"));
