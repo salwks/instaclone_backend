6d472ff44149effca5e5c8ea2aee5c6773f55e94
Sin Jaeho <stra2003@gmail.com>
2022-12-13 14:51:40 +0900

    2.Setup Graphql,Apollo,Nodemon,Babel

---
+++ server.js
@@ -0,0 +1,22 @@
+import { ApolloServer, gql } from "apollo-server";
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const mutation = {};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server
+  .listen()
+  .then(() => console.log("Server is running on https://localhost:4000"));
